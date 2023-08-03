49309c9a355d4d2077c0711d87f04181d755184c
vargasmichael <119553981+vargasmichael@users.noreply.github.com>
2023-08-03 09:01:09 -0600

    refactored and 0/3 tests passed

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const removeSNames = (names) => {
-  let expectedOutput = names;
-  names.forEach((element) => {
-    if (element.toLowerCase().charAt(0) === "s") {
-      const index = expectedOutput.indexOf(element);
-      expectedOutput.splice(index, 1);
-    }
-  });
-  return expectedOutput;
+  return names.filter((element) => element.toLowerCase().charAt(0) === "s");
+  //   let expectedOutput = names;
+  //   names.forEach((element) => {
+  //     if (element.toLowerCase().charAt(0) === "s") {
+  //       const index = expectedOutput.indexOf(element);
+  //       expectedOutput.splice(index, 1);
+  //     }
+  //   });
+  //   return expectedOutput;
 };
 
 module.exports = removeSNames;
